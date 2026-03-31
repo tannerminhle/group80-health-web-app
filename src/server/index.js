f94bcbd047786d6d927db995c7a7f6b6efd901d4
diwu033 <anthoning@hotmail.com>
2026-03-30 21:02:12 -0700

    Serve React static files from Express in production

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require("path");
 const router = require("./routes/router");
 const populationCache = require("./services/populationCache");
 
 const app = express();
 
 app.use("/api", router);
+
+// Serve React frontend in production
+const clientDist = path.join(__dirname, "../../src/client/dist");
+app.use(express.static(clientDist));
+
+// All non-API routes return index.html (supports React Router)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(clientDist, "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
